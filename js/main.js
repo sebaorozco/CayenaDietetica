da3e782627d93d8cdb7eab534cd6d240b501d04f
Sebastian Orozco <seba_orozco@hotmail.com>
2022-08-09 17:11:06 -0300

    2da entrega del proyecto

--- js/main.js
+++ js/main.js
@@ -51,7 +51,7 @@
 //         }else{
 //             console.log("Lo sentimos, no contamos con stock del producto seleccionado.");    
 //         }
-    
+
 //     console.log(carrito);
 // }
 
@@ -87,12 +87,12 @@
 // {id: 4, name: "Pasta de almendras s/gluten", price: 370, stock: 5}]
 
 // let carrito = []; 
-   
+
 // const miCompra = parseInt(prompt("Ingrese el id del producto que desea comprar: \n 1-Dulce de leche vegano. \n 2-Premezcla para Falafel. \n 3-Levadura nutricional. \n 4-Pasta de almendras s/gluten."));
 // const resultado = productos.find((el) => el.id === miCompra);
 
 // function agregarAlCarrito(producto, cantidadDeseada){
-     
+
 //     let miVenta = {
 //         producto: producto.name, stockActual: producto.stock, cantVendida: cantidadDeseada, costo: producto.price * cantidadDeseada
 //     }
@@ -101,7 +101,7 @@
 //     console.log("Usted agregó "+ miVenta.cantVendida +" "+ miVenta.producto + " a su carrito. El costo total es: $" + miVenta.costo);
 //     console.log(carrito);
 // }
-    
+
 // function validarStock(resultado, cantidadDeseada){
 //     if(resultado.stock > 0){
 //         if(cantidadDeseada<=resultado.stock){
@@ -133,18 +133,18 @@
 
 // let carrito = []; 
 
-   
+
 // const miCompra = parseInt(prompt("Ingrese el id del producto que desea comprar: \n 1-Dulce de leche vegano. \n 2-Premezcla para Falafel. \n 3-Levadura nutricional. \n 4-Pasta de almendras s/gluten."));
 // const resultado = productos.find((el) => el.id === miCompra);
 
 // function agregarAlCarrito(producto, cantidadDeseada){
-     
+
 //     let miVenta = {
 //         producto: producto.name, 
 //         stockActual: producto.stock, 
 //         cantVendida: cantidadDeseada, 
 //         costo: producto.price * cantidadDeseada}
-    
+
 
 //     carrito.push(miVenta); 
 //     let compra = document.querySelector("#botonCarrito");
@@ -154,7 +154,7 @@
 //     compra.appendChild(contenedor);
 // }
 
-    
+
 // function validarStock(resultado, cantidadDeseada){
 //     if(resultado.stock > 0){
 //         if(cantidadDeseada<=resultado.stock){
@@ -178,41 +178,89 @@
 //     agregarAlCarrito(resultado,cantidadDeseada);
 // }
 
-/*********************************Desafio Eventos******************************** */
-const productos = [ 
-    {id: 1, name: "Dulce de leche vegano", description: "Doña Magdalena.", price: 550, img: "images/Prod/dulce_leche_coco_vegan.jpg", stock: 10},
-    {id: 2, name: "Premezcla para Falafel", description: "Natural Pop.", price: 920, img:"images/Prod/falafel_natural_pop.jpg", stock: 15},
-    {id: 3, name: "Levadura nutricional", description: "Healthy Waw.", price: 489.99, img:"images/Prod/levadura_waw.jpg", stock: 20},
-    {id: 4, name: "Pasta de almendras s/gluten", description: "Vrink.", price: 370, img:"images/Prod/pasta_almendras_vrink.jpg" , stock: 5}
+/*********************************2da entrega del Proyecto******************************** */
+const productos = [
+    { id: 1, name: "Dulce de leche vegano", description: "Doña Magdalena.", price: 550, img: "images/Prod/dulce_leche_coco_vegan.jpg", stock: 10 },
+    { id: 2, name: "Premezcla para Falafel", description: "Natural Pop.", price: 920, img: "images/Prod/falafel_natural_pop.jpg", stock: 15 },
+    { id: 3, name: "Levadura nutricional", description: "Healthy Waw.", price: 489.99, img: "images/Prod/levadura_waw.jpg", stock: 20 },
+    { id: 4, name: "Pasta de almendras s/gluten", description: "Vrink.", price: 370, img: "images/Prod/pasta_almendras_vrink.jpg", stock: 5 }
 ]
 
-const carrito = JSON.parse(localStorage.getItem('carrito')) ?? []; 
+let total = 0;
+const carrito = JSON.parse(localStorage.getItem("carrito")) ?? [];
 document.getElementById("total-carrito").innerHTML = carrito.length;
 
-// Agregar cards de productos //
+
+// **********/ Agregar cards de productos /*************//
 productos.forEach((producto) => {
     const idButton = `add-cart${producto.id}`;
-    document.getElementById("section-cards").innerHTML += `<div class="col-lg-3 col-md-6 col-sm-12 card prod__card h-100" style="width: 25rem">
-    <img src=${producto.img} class="card-img-top prod__foto img-fluid" alt="Dulce de leche vegano">
-    <ul class="list-group list-group-flush">
-        <li class="list-group-item prod__descripcion">
-            <h3 class="card-title">${producto.name}</h3>
-        </li>
-        <li class="list-group-item prod__descripcion">${producto.description}</li>
-        <li class="list-group-item prod__precio">$${producto.price}</li>
-    </ul>
-    <div class="card-footer p-4 pt-0 mt-4 border-top-0 bg-transparent">
-        <div class="text-center"><a class="btn btn-outline-dark mt-auto boton__carrito" id="${idButton}">Agregar al carrito</a></div>
-    </div>
+    document.getElementById("section-cards").innerHTML +=
+        `<div class="col-lg-3 col-md-6 col-sm-12 card prod__card h-100" style="width: 25rem">
+        <img src=${producto.img} class="card-img-top prod__foto img-fluid" alt="">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item prod__descripcion">
+                <h3 class="card-title">${producto.name}</h3>
+            </li>
+            <li class="list-group-item prod__descripcion">${producto.description}</li>
+            <li class="list-group-item prod__precio">$${producto.price}</li>
+        </ul>
+        <div class="card-footer p-4 pt-0 mt-4 border-top-0 bg-transparent">
+            <div class="text-center">
+                <button class="btn btn-outline-dark mt-auto boton__carrito" id="${idButton}">Agregar al carrito</button>
+            </div>
+        </div>
     </div>`
 });
 
-// Agregar al carrito //
+//************/ Agregar al carrito /****************//
 productos.forEach((producto) => {
     const idButton = `add-cart${producto.id}`;
-    document.getElementById(idButton).addEventListener("click", () => {
-        carrito.push(producto);
+    document.getElementById(idButton).addEventListener("click", () => { 
+        const newItem = {
+            img: producto.img,
+            title: producto.name,
+            precio: producto.price,
+            cantidad: 1
+        } 
+        carrito.push(newItem);
         document.getElementById("total-carrito").innerHTML = carrito.length;
         localStorage.setItem("carrito", JSON.stringify(carrito));
-    })  
-})
+        modalCarrito(newItem);
+        totalCarrito(newItem);         
+    })
+})   
+
+
+//************/ Modal del carrito /****************//
+function modalCarrito(item) {
+    var content = document.getElementById("tbody").innerHTML +=
+                                `
+                                <th scope="row">1</th>
+                                <td class="table__productos">
+                                    <img src=${item.img} alt="">
+                                    <h6 class="title">${item.title}</h6>
+                                </td>
+                                <td class="table__precio"><p>$${item.precio}</p></td>
+                                <td class="table__cantidad">
+                                    <input type="number" min="1" value=${item.cantidad} id="input__elemento">
+                                    <button class="btn btn-transparent" id="delete">
+                                        <img class="trash" src="images/Icon/delete.png" alt="Icono eliminar item del carrito" />
+                                    </button>
+                                </td>`
+    // localStorage.setItem("modalCart", JSON.stringify(carrito));  
+}
+
+//************/ Acumulador del total a pagar del carrito /****************//
+function totalCarrito(item){
+    total = total + item.precio;
+    const itemCartTotal = document.querySelector('.itemCartTotal')
+    itemCartTotal.innerHTML = `<h3 class=" text-black">Total: $${total}</h3>`    
+}
+
+//************/ Eliminar producto del carrito /****************//
+
+
+        
+
+
+
