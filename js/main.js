2529b5d2d41bc4c3d37752a11c2e8e67edd1bfbb
Sebastian Orozco <seba_orozco@hotmail.com>
2022-08-16 17:31:26 -0300

    Incorporo libreria

--- js/main.js
+++ js/main.js
@@ -9,11 +9,18 @@ const productosDestacados = [
 const contentCarrito = document.getElementById("tbody");
 const botonVaciarCarrito = document.getElementById("vaciarCarrito");
 
+
+
+
+
 let carrito = [];
+
+window.swal = this.swal = this.Sweetalert2;
 
 document.addEventListener("DOMContentLoaded", () => {
     if (localStorage.getItem("carrito")){
         carrito = JSON.parse(localStorage.getItem("carrito"));
+        console.log(carrito);
         actualizarModalCarrito();
     }
 });
@@ -22,9 +29,27 @@ document.addEventListener("DOMContentLoaded", () => {
 //****************/ Vaciar el carrito /****************//
 
 botonVaciarCarrito.addEventListener("click", () => {
-    carrito.splice(0, carrito.length);
-    prod.cant = 0;
-    actualizarModalCarrito();
+    Swal.fire({
+        title: '¿Está seguro de vaciar el carrito?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Sí, seguro',
+        cancelButtonText: 'No, no quiero'
+    }).then((result) => {
+
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: 'Listo!',
+                icon: 'success',
+                text: 'Se ha vaciado el carrito'
+            })
+            carrito.splice(0, carrito.length);
+            console.log(carrito);
+            localStorage.removeItem("carrito");
+            actualizarModalCarrito();
+        }
+    })
+
 })
 
 // *********************/ Generar cards de Productos Destacados/************************//
@@ -54,34 +79,62 @@ productosDestacados.forEach((producto) => {
 productosDestacados.forEach((producto) => {
     const botonAgregar = document.getElementById(`add-cart${producto.id}`);
     botonAgregar.addEventListener("click", () => {
-            agregarAlCarrito(producto.id);
+
+        Swal.fire({
+            title: '¡Producto agregado!',
+            text: '',
+            icon: 'success',
+            confirmButtonText: 'OK'
+    })
+    
+        agregarAlCarrito(producto.id);
     })
 });
 
 // *****************/ Funcion Agregar al carrito /**********************//
 
 const agregarAlCarrito = (prodId) => {
+    
     const existeProd = carrito.some ((prod) => prodId === prod.id);
+    console.log(existeProd);
         
     if(existeProd){
         const prodActualizado = carrito.map((el) => {
-                prodId === el.id &&  el.cant++; 
+                prodId === el.id && el.cant++; 
         })
     } else {
+        console.log(carrito);
         const item = productosDestacados.find((prod) => prod.id === prodId);
         carrito.push(item);
     }
-
+    
     actualizarModalCarrito();
 }; 
 
 //************/ Eliminar producto del carrito /****************//
          
 const eliminarProd = (itemId) => {
-    const item = carrito.find((prod) => prod.id === itemId);
-    const indice = carrito.indexOf(item);
-    carrito.splice(indice, 1);
-    actualizarModalCarrito();
+    Swal.fire({
+        title: '¿Está seguro de eliminar el producto?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Sí, seguro',
+        cancelButtonText: 'No, no quiero'
+    }).then((result) => {
+
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: 'Borrado!',
+                icon: 'success',
+                text: 'El producto ha sido eliminado.'
+            })
+            const item = carrito.find((prod) => prod.id === itemId);
+            const indice = carrito.indexOf(item);
+            carrito.splice(indice, 1);
+            actualizarModalCarrito();
+        }
+    })
+
 };
 
 
@@ -102,7 +155,9 @@ const actualizarModalCarrito = () => {
                                     </td>
                                     <td class="table__precio"><p>$${prod.price}</p></td>
                                     <td class="table__cantidad">
-                                        <input type="number" min="1" value= ${prod.cant} id="cantidad">
+                                        <input type="number" value= "${prod.cant}">
+                                    </td>
+                                    <td>
                                         <button onclick="eliminarProd(${prod.id})" class="btn btn-transparent">
                                             <img  class="trash" src="images/Icon/delete.png" alt="Icono eliminar item del carrito" />
                                         </button>
@@ -110,15 +165,15 @@ const actualizarModalCarrito = () => {
     localStorage.setItem("carrito", JSON.stringify(carrito));
     })  
     document.getElementById("total-carrito").innerText = carrito.length;
-    document.getElementById("precioTotal").innerText = carrito.reduce((acc,el) => acc + el.price, 0);
-}
+    document.getElementById("precioTotal").innerText = carrito.reduce((acc,el) => acc + (el.price*el.cant), 0);
+};
 
-//************/ Acumulador del total a pagar del carrito /****************//
-function totalCarrito(item){
-    total += item.precio;
-    const itemCartTotal = document.querySelector('.itemCartTotal')
-    itemCartTotal.innerHTML = `<h3 class=" text-black">Total: $${total}</h3>`    
-}
-
-
-
+//************/ Actualizar input cantidad del carrito /****************//
+// onchange="cambiarInputCantidad()"
+// const cambiarInputCantidad = (e) => {
+//     const input = e.target;
+//     if(isNaN(input.value) || input.value <= 0){
+//         input.value = 1;
+//     }             
+//     actualizarModalCarrito();
+// }
