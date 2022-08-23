632250b8b09121e082c401dfd13936716a2853f3
Sebastian Orozco <seba_orozco@hotmail.com>
2022-08-23 00:59:43 -0300

    Fetch en mi proyecto

--- js/main.js
+++ js/main.js
@@ -1,26 +1,14 @@
-//*********************************/ 2da entrega del Proyecto /******************************** *//
-const productosDestacados = [
-    { id: 1, name: "Dulce de leche vegano", marca: "Doña Magdalena.", price: 550, img: "images/Prod/dulce_leche_coco_vegan.jpg", stock: 10, cant: 1},
-    { id: 2, name: "Premezcla para Falafel", marca: "Natural Pop.", price: 920, img: "images/Prod/falafel_natural_pop.jpg", stock: 15, cant: 1},
-    { id: 3, name: "Levadura nutricional", marca: "Healthy Waw.", price: 489.99, img: "images/Prod/levadura_waw.jpg", stock: 20, cant:1},
-    { id: 4, name: "Pasta de almendras s/gluten", marca: "Vrink.", price: 370, img: "images/Prod/pasta_almendras_vrink.jpg", stock: 5, cant:1}
-]
+//*********************************/ Agregando fetch al proyecto /******************************** *//
 
 const contentCarrito = document.getElementById("tbody");
 const botonVaciarCarrito = document.getElementById("vaciarCarrito");
 
-
-
-
-
 let carrito = [];
-
-window.swal = this.swal = this.Sweetalert2;
 
 document.addEventListener("DOMContentLoaded", () => {
     if (localStorage.getItem("carrito")){
         carrito = JSON.parse(localStorage.getItem("carrito"));
-        console.log(carrito);
+      
         actualizarModalCarrito();
     }
 });
@@ -52,44 +40,60 @@ botonVaciarCarrito.addEventListener("click", () => {
 
 })
 
+
 // *********************/ Generar cards de Productos Destacados/************************//
-productosDestacados.forEach((producto) => {
-    document.getElementById("cards-productos-destacados").innerHTML +=
-        `<div class="card col-lg-4 col-md-3 col-sm-12 prod__card h-100" style="width: 25rem">
-            <img src=${producto.img} class="card-img-top prod__foto img-fluid" alt="">
-            <div class="card-body">
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item prod__descripcion">
-                        <h3 class="card-title">${producto.name}</h3>
-                    </li>
-                    <li class="list-group-item prod__descripcion">${producto.marca}</li>
-                    <li class="list-group-item prod__precio">$${producto.price}</li>
-                </ul>
-            </div>
-            <div class="card-footer p-4 pt-0 mt-4 border-top-0 text-center">
-                    <button id="add-cart${producto.id}" class="btn btn-outline-dark mt-auto boton__carrito">
-                    Agregar al carrito
-                    </button>    
-            </div>
-        </div>`       
-});
+const generarCardsDestacados = () => {
+    fetch("productosDestacados.json")
+        .then((response) => response.json())
+        .then((informacion) => {
+            let acumulador = ``;
+            informacion.forEach((producto) =>{
+                acumulador += `<div class="card col-lg-4 col-md-3 col-sm-12 prod__card h-100" style="width: 25rem">
+                    <img src=${producto.img} class="card-img-top prod__foto img-fluid" alt="">
+                    <div class="card-body">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item prod__descripcion">
+                            <h3 class="card-title">${producto.name}</h3>
+                            </li>
+                            <li class="list-group-item prod__descripcion">${producto.marca}</li>
+                            <li class="list-group-item prod__precio">$${producto.price}</li>
+                        </ul>
+                    </div>
+                    <div class="card-footer p-4 pt-0 mt-4 border-top-0 text-center">
+                        <button id="add-cart${producto.id}" class="btn btn-outline-dark mt-auto boton__carrito">
+                        Agregar al carrito
+                        </button>    
+                    </div>
+                </div>`
+                
+            })
+            document.getElementById("cards-productos-destacados").innerHTML = acumulador;
+        })
+}
+
+generarCardsDestacados();
 
 // *********************/ Llamar a la funcion Agregar al Carrito/************************//
-
-productosDestacados.forEach((producto) => {
-    const botonAgregar = document.getElementById(`add-cart${producto.id}`);
-    botonAgregar.addEventListener("click", () => {
-
-        Swal.fire({
-            title: '¡Producto agregado!',
-            text: '',
-            icon: 'success',
-            confirmButtonText: 'OK'
-    })
+fetch("productosDestacados.json")
+    .then((resp) => resp.json())
+    .then(data => {
+        productosDestacados = data;
+        productosDestacados.forEach((producto) => {
+            const botonAgregar = document.getElementById(`add-cart${producto.id}`);
+            botonAgregar.addEventListener("click", () => {
+        
+                Swal.fire({
+                    title: '¡Producto agregado!',
+                    text: '',
+                    icon: 'success',
+                    confirmButtonText: 'OK'
+                })
+        
+                agregarAlCarrito(producto.id);
+            })
+        });
     
-        agregarAlCarrito(producto.id);
     })
-});
 
 // *****************/ Funcion Agregar al carrito /**********************//
 
@@ -100,10 +104,18 @@ const agregarAlCarrito = (prodId) => {
         
     if(existeProd){
         const prodActualizado = carrito.map((el) => {
-                prodId === el.id && el.cant++; 
+            
+            return prodId === el.id ? {id: el.id, 
+                                        name: el.name, 
+                                        marca: el.marca, 
+                                        price: el.price, 
+                                        img: el.img, 
+                                        stock: el.stock,
+                                        cant: el.cant+1}:el; 
         })
+        carrito = prodActualizado;
+        
     } else {
-        console.log(carrito);
         const item = productosDestacados.find((prod) => prod.id === prodId);
         carrito.push(item);
     }
@@ -155,7 +167,7 @@ const actualizarModalCarrito = () => {
                                     </td>
                                     <td class="table__precio"><p>$${prod.price}</p></td>
                                     <td class="table__cantidad">
-                                        <input type="number" value= "${prod.cant}">
+                                        <input type="number" min="1" value= "${prod.cant}" oninput="sumaCantidad(${prod.id})">
                                     </td>
                                     <td>
                                         <button onclick="eliminarProd(${prod.id})" class="btn btn-transparent">
@@ -166,14 +178,36 @@ const actualizarModalCarrito = () => {
     })  
     document.getElementById("total-carrito").innerText = carrito.length;
     document.getElementById("precioTotal").innerText = carrito.reduce((acc,el) => acc + (el.price*el.cant), 0);
+    
+    
 };
 
 //************/ Actualizar input cantidad del carrito /****************//
-// onchange="cambiarInputCantidad()"
-// const cambiarInputCantidad = (e) => {
-//     const input = e.target;
-//     if(isNaN(input.value) || input.value <= 0){
-//         input.value = 1;
-//     }             
-//     actualizarModalCarrito();
-// }
+const sumaCantidad = (itemId) => {
+    const item = carrito.find((prod) => prod.id === itemId);
+    const indice = carrito.indexOf(item);
+    const nuevaCant = parseInt(document.getElementsByTagName("input")[indice].value);
+       
+    const cantActualizado = carrito.map((el) => {
+        
+        return itemId === el.id ? {id: el.id, 
+                                    name: el.name, 
+                                    marca: el.marca, 
+                                    price: el.price, 
+                                    img: el.img, 
+                                    stock: el.stock,
+                                    cant: nuevaCant}:el; 
+    })
+    if(isNaN(nuevaCant) || nuevaCant < 0){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'No ingresaste una cantidad válida!',
+            footer: 'Intenta de nuevo.'
+          })
+        
+    }else{
+        carrito = cantActualizado;
+        actualizarModalCarrito();
+    }
+}
